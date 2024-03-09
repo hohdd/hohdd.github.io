90319f68ef0937f6773c9ff0ccedf9eb4399ca45
Ho Duc Dong <donghd@nec.vn>
2024-03-09 11:51:30 +0700

    09032024

--- tools/glsl_editor/listShaderBookmarks.js
+++ tools/glsl_editor/listShaderBookmarks.js
@@ -1,62 +1,64 @@
 const itemsList = [
-    // --- GROUP: "Inigo Quiles"
+    // --- GROUP: "Template"
     {
         group: "Template",
-        src: "content/simple_function.frag",
+        src: "/contents/glsl/simple_function.frag",
         title: "Simple Function",
         description: "Mẫu để kiểm tra 'Simple Function'"
     },
     {
         group: "Template",
-        src: "content/tryit_function.frag",
+        src: "/contents/glsl/tryit_function.frag",
         title: "Try some function playground",
         description: "Khám phá một số hàm mũ và đa thức"
     },
+    // --- GROUP: "DongHD"
     {
         group: "DongHD",
-        src: "content/dhd/dhd_art_01.frag",
+        src: "/contents/glsl/dhd/dhd_art_01.frag",
         title: "Hoa văn 01",
         description: "Hoa văn màu sắc animation"
     },
+    // --- GROUP: "Inigo Quiles"
     {
         group: "Inigo Quiles",
-        src: "content/impulse.frag",
+        src: "/contents/glsl/impulse.frag",
         title: "Impulse",
         description: "impulse by Inigo Quiles"
     },
     {
         group: "Inigo Quiles",
-        src: "content/cubicpulse.frag",
+        src: "/contents/glsl/cubicpulse.frag",
         title: "cubicpulse",
         description: ""
     },
     {
         group: "Inigo Quiles",
-        src: 'content/expo.frag',
+        src: '/contents/glsl/expo.frag',
         title: "expo.frag",
         description: ""
     },
     {
         group: "Inigo Quiles",
-        src: 'content/expstep.frag',
+        src: '/contents/glsl/expstep.frag',
         title: "expstep.frag",
         description: ""
     },
     {
         group: "Inigo Quiles",
-        src: 'content/impulse.frag',
+        src: '/contents/glsl/impulse.frag',
         title: "impulse.frag",
         description: ""
     },
     {
         group: "Inigo Quiles",
-        src: 'content/parabola.frag',
+        src: '/contents/glsl/parabola.frag',
         title: "parabola.frag",
         description: ""
     },
     {
         group: "Inigo Quiles",
-        src: 'content/pcurve.frag',
+        src: '/contents/glsl/pcurve.frag',
         title: "pcurve.frag",
         description: ""
     },
